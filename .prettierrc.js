1591136cd2799afd2faf0b3ab7a2aa699724a0e5
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2022-05-10 11:03:20 +0200

    :sparkles: ZombieFactory

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all', // https://prettier.io/docs/en/options.html#trailing-commas
+  bracketSpacing: true, // https://prettier.io/docs/en/options.html#bracket-spacing
+  bracketSameLine: false, // https://prettier.io/docs/en/options.html#bracket-line
+  arrowParens: 'always', // https://prettier.io/docs/en/options.html#arrow-function-parentheses
+  proseWrap: 'never', // https://prettier.io/docs/en/options.html#prose-wrap
+  htmlWhitespaceSensitivity: 'strict', // https://prettier.io/docs/en/options.html#html-whitespace-sensitivity
+  endOfLine: 'lf', // https://prettier.io/docs/en/options.html#end-of-line
+};
